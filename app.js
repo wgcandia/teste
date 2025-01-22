581da9941e7c2afdd84e2b6dec376e3259f624e6
Junior <twojac@gmail.com>
2025-01-22 17:00:10 -0300

    alterado para 10

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 // função para diminuir linhas de codigos 
 let tentativas = 1;
@@ -19,7 +19,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1' , 'Jogo do Número secreto');
-    exibirTextoNaTela('p' , 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p' , 'Escolha um número entre 1 e 10');
 }
 exibirMensagemInicial();
 function verificarChute(){
